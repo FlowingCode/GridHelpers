504658f3b651ab9c3618ecc04cedebdf81bdf8d1
Martín López <mlopez@flowingcode.com>
2022-06-13 11:04:40 -0300

    feat: add support for obtaining headers and footers contents Close #2

---
+++ src/test/java/com/flowingcode/vaadin/addons/gridhelpers/GetHeaderFooterDemo.java
@@ -0,0 +1,45 @@
+package com.flowingcode.vaadin.addons.gridhelpers;
+
+import com.flowingcode.vaadin.addons.demo.DemoSource;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.Grid.Column;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.PageTitle;
+
+@PageTitle("Get Header or Footer")
+@DemoSource(
+    "https://github.com/FlowingCode/GridHelpers/blob/master/src/test/java/com/flowingcode/vaadin/addons/gridhelpers/GetHeaderFooterDemo.java")
+public class GetHeaderFooterDemo extends Div {
+
+  public GetHeaderFooterDemo() {
+    setSizeFull();
+
+    Grid<Person> grid = new Grid<>();
+    grid.setItems(TestData.initializeData());
+
+    Column<Person> firstName = grid.addColumn(Person::getFirstName).setHeader("First name").setFooter("First name footer");
+    Column<Person> lastName = grid.addColumn(Person::getLastName).setHeader("Last name").setFooter("Last name footer");;
+    Column<Person> country = grid.addColumn(Person::getCountry).setHeader("Country").setFooter("Country footer");;
+
+    HorizontalLayout hl = new HorizontalLayout(new Button(VaadinIcon.TOOLS.create(),ev->Notification.show("Not implemented")));
+    hl.setSizeFull();
+    hl.setJustifyContentMode(JustifyContentMode.END);
+
+    Button getHeaders = new Button("Obtain headers", ev->Notification.show(
+        "Header text column 1: " + GridHelper.getHeader(grid,firstName) + 
+        ", Header text column 2: " + GridHelper.getHeader(grid,lastName) +
+        ", Header text column 3: " + GridHelper.getHeader(grid,country)));
+    Button getFooters = new Button("Obtain footers", ev->Notification.show(
+        "Header text column 1: " + GridHelper.getFooter(grid,firstName) + 
+        ", Header text column 2: " + GridHelper.getFooter(grid,lastName) +
+        ", Header text column 3: " + GridHelper.getFooter(grid,country)));
+
+    grid.setHeightFull();
+    add(grid, getHeaders, getFooters);
+  }
+}
