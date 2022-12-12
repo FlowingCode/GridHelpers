fc2310146acf230a88d123c2256b930328d5d256
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2022-12-12 09:55:18 -0300

    fix: support vaadin getHeader/getFooter
    Close #33

--- src/main/java/com/flowingcode/vaadin/addons/gridhelpers/HeaderFooterGridHelper.java
+++ src/main/java/com/flowingcode/vaadin/addons/gridhelpers/HeaderFooterGridHelper.java
@@ -1,10 +1,12 @@
 package com.flowingcode.vaadin.addons.gridhelpers;
 
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.Grid.Column;
+import com.vaadin.flow.data.renderer.Renderer;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.data.renderer.Renderer;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @SuppressWarnings("serial")
@@ -12,6 +14,18 @@ import lombok.RequiredArgsConstructor;
 class HeaderFooterGridHelper implements Serializable {
 
   private final GridHelper<?> helper;
+
+  private static final Method getHeaderText = getMethod(Column.class, "getHeaderText").orElse(null);
+
+  private static final Method getFooterText = getMethod(Column.class, "getFooterText").orElse(null);
+
+  private static Optional<Method> getMethod(Class<?> clazz, String name) {
+    try {
+      return Optional.of(clazz.getMethod(name));
+    } catch (NoSuchMethodException e) {
+      return Optional.empty();
+    }
+  }
 
   private static String getTemplate(Renderer<?> renderer) {
       try {
@@ -50,13 +64,31 @@ class HeaderFooterGridHelper implements Serializable {
    * See https://github.com/vaadin/flow-components/issues/1496
    */
   public String getHeader(Grid.Column<?> column) {
-      final Renderer<?> headerRenderer = getHeaderRenderer(column);
+    if (getHeaderText != null) {
+      // Vaadin 23.2+
+      try {
+        return (String) getHeaderText.invoke(column);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      Renderer<?> headerRenderer = getHeaderRenderer(column);
       return getTemplate(headerRenderer);
+    }
   }
 
   public String getFooter(Grid.Column<?> column) {
-      final Renderer<?> headerRenderer = getFooterRenderer(column);
+    if (getFooterText != null) {
+      // Vaadin 23.2+
+      try {
+        return (String) getFooterText.invoke(column);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    } else {
+      Renderer<?> headerRenderer = getFooterRenderer(column);
       return headerRenderer==null?null:getTemplate(headerRenderer);
+    }
   }
 
 }
