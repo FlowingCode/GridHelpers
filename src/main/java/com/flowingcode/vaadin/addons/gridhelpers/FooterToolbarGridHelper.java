6b40a4b79eb3efb04e7d470fe1542c83d883911e
Felipe Lang <felipe.lang@ext.vaadin.com>
2023-01-03 09:36:13 -0300

    fix: allow setting grid footer toolbar over an existing one
    Close #50

--- src/main/java/com/flowingcode/vaadin/addons/gridhelpers/FooterToolbarGridHelper.java
+++ src/main/java/com/flowingcode/vaadin/addons/gridhelpers/FooterToolbarGridHelper.java
@@ -1,10 +1,10 @@
 package com.flowingcode.vaadin.addons.gridhelpers;
 
-import java.io.Serializable;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.grid.FooterRow;
 import com.vaadin.flow.component.grid.FooterRow.FooterCell;
 import com.vaadin.flow.component.grid.Grid;
+import java.io.Serializable;
 import lombok.RequiredArgsConstructor;
 
 @SuppressWarnings("serial")
@@ -13,17 +13,22 @@ class FooterToolbarGridHelper implements Serializable {
 
   private final GridHelper<?> helper;
 
+  private FooterCell footerCell;
+
   public void setFooterToolbar(Component toolBar) {
     Grid<?> grid = helper.getGrid();
     if (grid.getFooterRows().isEmpty()) {
-      // create a fake footer and hide it (workaround: https://github.com/vaadin/flow-components/issues/1558#issuecomment-987783794)
+      // create a fake footer and hide it (workaround:
+      // https://github.com/vaadin/flow-components/issues/1558#issuecomment-987783794)
       grid.appendFooterRow();
       grid.getElement().getThemeList().add("hide-first-footer");
     }
-    FooterRow fr = grid.appendFooterRow();
-    FooterCell fc = fr.join(grid.getColumns().toArray(new Grid.Column[0]));
-    toolBar.getElement().setAttribute("fcGh-footer",true);
-    fc.setComponent(toolBar);
+    if (footerCell == null) {
+      FooterRow fr = grid.appendFooterRow();
+      footerCell = fr.join(grid.getColumns().toArray(new Grid.Column[0]));
+    }
+    toolBar.getElement().setAttribute("fcGh-footer", true);
+    footerCell.setComponent(toolBar);
   }
 
 }
