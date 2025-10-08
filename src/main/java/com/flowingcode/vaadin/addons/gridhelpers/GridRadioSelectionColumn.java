63a138b08c71695fc4dc0098e237b2c2684bc003
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2025-10-08 09:15:59 -0300

    WIP: remove NpmPackage import

--- src/main/java/com/flowingcode/vaadin/addons/gridhelpers/GridRadioSelectionColumn.java
+++ src/main/java/com/flowingcode/vaadin/addons/gridhelpers/GridRadioSelectionColumn.java
@@ -26,7 +26,6 @@ import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 
