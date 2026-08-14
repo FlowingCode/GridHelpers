19331251168a0a7bcca147bbda44ef567b50eca8
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2026-08-14 15:35:49 -0300

    feat(demo): enable dynamic theme

---
+++ src/test/java/com/flowingcode/vaadin/addons/AppShellConfiguratorImpl.java
@@ -0,0 +1,35 @@
+/*-
+ * #%L
+ * Grid Helpers Add-on
+ * %%
+ * Copyright (C) 2022 - 2026 Flowing Code
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.flowingcode.vaadin.addons;
+
+import com.flowingcode.vaadin.addons.demo.DynamicTheme;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.AppShellSettings;
+
+public class AppShellConfiguratorImpl implements AppShellConfigurator {
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        if (DynamicTheme.isFeatureSupported()) {
+            DynamicTheme.LUMO.initialize(settings);
+        }
+    }
+
+}
